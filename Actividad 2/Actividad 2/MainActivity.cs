d0c6c695d48561db7d7e8a4d93765945d4721eec
MAURICIO NEPOMUCENO DE FLORENCIO <mauricio41314@comunidad.unam.mx>
2016-02-26 11:50:06 -0600

    Cambie el hola mundo por mi nombre y número de cuenta

---
+++ Actividad 2/Actividad 2/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace HolaMundo
+{
+    public class App
+    {
+        public static Page GetMainPage()
+        {
+            return new ContentPage
+            {
+                Content = new Label
+                {
+                    Text = "Nombre: Mauricio Nepomuceno De Florencio Número de cuenta: 413140698 ",
+                    VerticalOptions = LayoutOptions.CenterAndExpand,
+                    HorizontalOptions = LayoutOptions.CenterAndExpand,
+                },
+            };
+        }
+    }
+}
